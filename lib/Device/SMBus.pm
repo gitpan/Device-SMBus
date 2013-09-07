4b48c20c1276a29282339e37b816653f22e5c684
Shantanu Bhadoria <shantanu.bhadoria@gmail.com>
2013-09-07 19:30:39 +0000

    Import of SHANTANU/Device-SMBus-0.02 from CPAN.
    gitpan-cpan-distribution: Device-SMBus
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SHANTANU/Device-SMBus-0.02.tar.gz
    gitpan-cpan-author:       SHANTANU
    gitpan-cpan-maturity:     released

---
+++ lib/Device/SMBus.pm
@@ -0,0 +1,192 @@
+use strict;
+use warnings;
+
+package Device::SMBus;
+
+# PODNAME: Device::SMBus
+# ABSTRACT: Perl interface for smbus using libi2c-dev library.
+#
+# This file is part of Device-SMBus
+#
+# This software is copyright (c) 2013 by Shantanu Bhadoria.
+#
+# This is free software; you can redistribute it and/or modify it under
+# the same terms as the Perl 5 programming language system itself.
+#
+our $VERSION = '0.02'; # VERSION
+
+use 5.010000;
+
+# Dependencies
+use Mo;
+use Carp;
+
+use IO::File;
+use Fcntl;
+
+require XSLoader;
+XSLoader::load( 'Device::SMBus', $VERSION );
+
+use constant I2C_SLAVE => 0x0703;
+
+
+has I2CBusDevicePath => ( is => 'ro', );
+
+has I2CBusFileHandle => (
+    is         => 'ro',
+    lazy_build => 1,
+);
+
+
+has I2CDeviceAddress => ( is => 'ro', );
+
+has I2CBusFilenumber => ( is => 'ro', );
+
+sub _build_I2CBusFileHandle {
+    my ($self) = @_;
+    my $fh = IO::File->new( $self->I2CBusDevicePath, O_RDWR );
+    if ( !$fh ) {
+        croak "Unable to open I2C Device File at $self->I2CBusDevicePath";
+        return -1;
+    }
+    $self->I2CBusFilenumber( $fh->fileno() );
+    $fh->ioctl( I2C_SLAVE, $self->I2CDeviceAddress );
+    return $fh;
+}
+
+
+sub readByteData {
+    my ( $self, $register_address ) = @_;
+    my $retval = Device::SMBus::_readByteData( $self->I2CBusFilenumber,
+        $register_address );
+}
+
+
+sub writeByteData {
+    my ( $self, $register_address, $value ) = @_;
+    my $retval =
+      Device::SMBus::_readByteData( $self->I2CBusFilenumber, $register_address,
+        $value );
+}
+
+sub DEMOLISH {
+    my ($self) = @_;
+    $self->I2CBusFileHandle->close();
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Device::SMBus - Perl interface for smbus using libi2c-dev library.
+
+=head1 VERSION
+
+version 0.02
+
+=head1 SYNOPSIS
+
+   use Device::SMBus;
+   $dev = Device::SMBus->new(
+     I2CBusDevicePath => '/dev/i2c-1',
+     I2CDeviceAddress => 0x1e,
+   );
+   print $dev->readByteData(0x20);
+
+=head1 DESCRIPTION
+
+This is a perl interface to smbus interface using libi2c-dev library. 
+
+=head1 ATTRIBUTES
+
+=head2 I2CBusDevicePath
+
+Device path of the I2C Device. 
+
+ * On Raspberry Pi Model A this would usually be /dev/i2c-0 if you are using the default pins.
+ * On Raspberry Pi Model B this would usually be /dev/i2c-1 if you are using the default pins.
+
+=head2 I2CDeviceAddress
+
+This is the Address of the device on the I2C bus, this is usually available in the device Datasheet.
+
+ * for /dev/i2c-0 look at output of `sudo i2cdetect -y 0' 
+ * for /dev/i2c-1 look at output of `sudo i2cdetect -y 1' 
+
+=head1 METHODS
+
+=head2 readByteData
+
+$self->readByteData($register_address)
+
+=head2 writeByteData
+
+$self->writeByteData($register_address,$value)
+
+=head1 USAGE
+
+=over
+
+=item *
+
+This module provides a simplified object oriented interface to the libi2c-dev library for accessing electronic peripherals connected on the I2C bus. It uses Mo, a scaled down version of Moose without any data checks to improve speed.
+
+=back
+
+=head1 see ALSO
+
+=over
+
+=item *
+
+L<Mo>
+
+=item *
+
+L<IO::File>
+
+=item *
+
+L<Fcntl>
+
+=back
+
+=for :stopwords cpan testmatrix url annocpan anno bugtracker rt cpants kwalitee diff irc mailto metadata placeholders metacpan
+
+=head1 SUPPORT
+
+=head2 Bugs / Feature Requests
+
+Please report any bugs or feature requests through github at 
+L<https://github.com/shantanubhadoria/device-smbus/issues>.
+You will be notified automatically of any progress on your issue.
+
+=head2 Source Code
+
+This is open source software.  The code repository is available for
+public review and contribution under the terms of the license.
+
+L<https://github.com/shantanubhadoria/device-smbus>
+
+  git clone git://github.com/shantanubhadoria/device-smbus.git
+
+=head1 AUTHOR
+
+Shantanu Bhadoria <shantanu at cpan dott org>
+
+=head1 CONTRIBUTOR
+
+Shantanu Bhadoria <shantanu@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Shantanu Bhadoria.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
